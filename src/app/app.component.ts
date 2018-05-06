781610b45332c4860ca84c49a068ce5bb869b96f
Taremeh <tarekalakmeh@hotmail.ch>
2018-05-06 16:12:47 +0200

    Apphoven Web App Initial Version
    Initial, working web version. Connected to Firebase Firestore through
    AngularFire 2. Using Angluar 6.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { DomSanitizer } from "@angular/platform-browser"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  private urlPathname;
+  private user;
+  private userAwards;
+  private videoUrl;
+
+  private itemDoc: AngularFirestoreDocument<any>;
+  item: Observable<any>;
+
+  constructor(private afs: AngularFirestore, private sanitizer: DomSanitizer) {
+    this.urlPathname = window.location.pathname.substr(1);
+    console.log("URL " + this.urlPathname);
+
+    let query = afs.collection('user', ref => ref.where('userUrl', '==', this.urlPathname)).valueChanges();
+    let result = query.subscribe(queriedItems => {
+      if(queriedItems.length > 0) {
+
+        console.log("RESULT: " + JSON.stringify(queriedItems));
+        console.log(queriedItems[0]);  
+        this.user = queriedItems[0];
+
+        // If userAwards exist => Split into Array
+        if(this.user.userAwards != ""){
+          this.userAwards = [];
+          this.userAwards = this.user.userAwards.split(",");
+        } else {
+          this.userAwards = false;
+        }
+
+        // If YT-Video-ID exists, bypass security
+        if(this.user.userVideoLink != ""){
+          let videoLink = 'https://www.youtube.com/embed/' + this.user.userVideoLink;
+          this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoLink);
+        } else {
+          this.videoUrl = false;
+        }
+        
+      } else {
+        console.log("user not found");
+      }
+      
+    });
+
+    // this.items = afs.collection('user/'+this.urlPathname).valueChanges();
+    // this.items = afs.collection('user/'+this.urlPathname).valueChanges();
+    // this.itemDoc = afs.doc<any>('user/1MKg1ouhlXdjO8YeGQpiq9k48tg2');
+    // this.item = this.itemDoc.valueChanges();
+
+    /*let query2 = afs.collection('user', ref => ref.where('piece.*.', '==', 1708)).valueChanges();
+    let result2 = query2.subscribe(queriedItems2 => {
+      console.log("RESULT 1212 " + queriedItems2);
+      if(queriedItems2.length > 0) {
+        console.log("RESULT22222: " + JSON.stringify(queriedItems2));
+        console.log(queriedItems2[0]);
+      }
+    });*/
+  }
+
+  ngOnInit(){
+    console.log(window.location.pathname);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
   ],
   providers: [],
   bootstrap: [AppComponent]

7320080f7a85cdab83d4e888b98c536181d9e6d4
Taremeh <tarekalakmeh@hotmail.ch>
2018-05-10 23:11:07 +0200

    UI Update
    Added default and 404 page. Added icons (fontawesome). UI-Update for
    Award-Section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,75 +1,129 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { DomSanitizer } from "@angular/platform-browser"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+	title = 'app';
 
-  private urlPathname;
-  private user;
-  private userAwards;
-  private videoUrl;
+	public urlPathname;
+	public user;
+	public userAwards;
+	public userAwardsInput: Array<any>;
+	public videoUrl;
+	public dataLoaded;
 
-  private itemDoc: AngularFirestoreDocument<any>;
-  item: Observable<any>;
+	constructor(private afs: AngularFirestore, private sanitizer: DomSanitizer, private _ngZone: NgZone) {
+		this.user = {profileIsPublic: false};
 
-  constructor(private afs: AngularFirestore, private sanitizer: DomSanitizer) {
-    this.urlPathname = window.location.pathname.substr(1);
-    console.log("URL " + this.urlPathname);
+		this.urlPathname = window.location.pathname.substr(1);
+		console.log("URL " + this.urlPathname);
+		
+		if(this.urlPathname == ""){
+			// root path "/"
+			this.urlPathname = false;
+			this.user = {profileIsPublic: false};
+			this.dataLoaded = true;
+		} else {
+			let query = afs.collection('user', ref => ref.where('userUrl', '==', this.urlPathname)).valueChanges();
+			let result = query.subscribe(queriedItems => {
+				if(queriedItems.length > 0) {
 
-    let query = afs.collection('user', ref => ref.where('userUrl', '==', this.urlPathname)).valueChanges();
-    let result = query.subscribe(queriedItems => {
-      if(queriedItems.length > 0) {
+					console.log("RESULT: " + JSON.stringify(queriedItems));
+					console.log(queriedItems[0]);  
+					this.user = queriedItems[0];
 
-        console.log("RESULT: " + JSON.stringify(queriedItems));
-        console.log(queriedItems[0]);  
-        this.user = queriedItems[0];
+					this.initUser();
+					
+				} else {
+					// User not found
+					this.urlPathname = false;
+					this.user = {profileIsPublic: false};
+					this.dataLoaded = true;
+					console.log("user not found");
+				}
+			});
+		}
 
-        // If userAwards exist => Split into Array
-        if(this.user.userAwards != ""){
-          this.userAwards = [];
-          this.userAwards = this.user.userAwards.split(",");
-        } else {
-          this.userAwards = false;
-        }
+		// this.items = afs.collection('user/'+this.urlPathname).valueChanges();
+		// this.items = afs.collection('user/'+this.urlPathname).valueChanges();
+		// this.itemDoc = afs.doc<any>('user/1MKg1ouhlXdjO8YeGQpiq9k48tg2');
+		// this.item = this.itemDoc.valueChanges();
 
-        // If YT-Video-ID exists, bypass security
-        if(this.user.userVideoLink != ""){
-          let videoLink = 'https://www.youtube.com/embed/' + this.user.userVideoLink;
-          this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoLink);
-        } else {
-          this.videoUrl = false;
-        }
-        
-      } else {
-        console.log("user not found");
-      }
-      
-    });
+		/*let query2 = afs.collection('user', ref => ref.where('piece.*.', '==', 1708)).valueChanges();
+		let result2 = query2.subscribe(queriedItems2 => {
+			console.log("RESULT 1212 " + queriedItems2);
+			if(queriedItems2.length > 0) {
+				console.log("RESULT22222: " + JSON.stringify(queriedItems2));
+				console.log(queriedItems2[0]);
+			}
+		});*/
+	}
 
-    // this.items = afs.collection('user/'+this.urlPathname).valueChanges();
-    // this.items = afs.collection('user/'+this.urlPathname).valueChanges();
-    // this.itemDoc = afs.doc<any>('user/1MKg1ouhlXdjO8YeGQpiq9k48tg2');
-    // this.item = this.itemDoc.valueChanges();
+	ngOnInit(){
+		console.log(window.location.pathname);
+	}
 
-    /*let query2 = afs.collection('user', ref => ref.where('piece.*.', '==', 1708)).valueChanges();
-    let result2 = query2.subscribe(queriedItems2 => {
-      console.log("RESULT 1212 " + queriedItems2);
-      if(queriedItems2.length > 0) {
-        console.log("RESULT22222: " + JSON.stringify(queriedItems2));
-        console.log(queriedItems2[0]);
-      }
-    });*/
-  }
+	initUser(){
 
-  ngOnInit(){
-    console.log(window.location.pathname);
-  }
+		// If userAwards exist => Split into Array
+		if(this.user.userAwards != ""){
+			this.userAwards = [];
+			this.userAwardsInput = [];
+			this.userAwardsInput = String(this.user.userAwards).split(",");
+
+
+				for(let i = 0; i < this.userAwardsInput.length; i++) {
+				let awardYear = 0;
+
+				// Search String for Year (19XX / 20XX)
+				if(this.userAwardsInput[i].match(/\b(19|20)\d{2}\b/g)){
+					awardYear = this.userAwardsInput[i].match(/\b(19|20)\d{2}\b/g)
+					console.log("MATCH in AWARD nr " + i + " with value: " + awardYear);
+				}
+
+				if(this.userAwardsInput[i] != ""){
+					if(this.userAwardsInput[i].includes(" - ")){
+						// Piano Competition 2018 - 1st Prize
+						let userAward = this.userAwardsInput[i].split(" - ");
+						this.userAwards.push({
+							competition: userAward[0],
+							prize: userAward[1],
+							year: awardYear
+						});
+					} else {
+						this.userAwards.push({
+							competition: this.userAwardsInput[i],
+							prize: false,
+							year: awardYear
+						});
+					}
+				}
+
+				// Sort Awards by year (descending)
+				this.userAwards.sort((a, b) => {
+					return b.year - a.year;
+				});
+
+				this.dataLoaded = true;
+			}
+		} else {
+			this.userAwards = false;
+			this.dataLoaded = true;
+		}
+
+		// If YT-Video-ID exists, bypass security
+		if(this.user.userVideoLink != ""){
+			let videoLink = 'https://www.youtube.com/embed/' + this.user.userVideoLink;
+			this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoLink);
+		} else {
+			this.videoUrl = false;
+		}
+	}
 
 }
